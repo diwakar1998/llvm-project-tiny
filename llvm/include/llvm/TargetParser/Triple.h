65f9c8ba91f55f980dbc9171c10d9c5cada8d3ff
Hikari_no_Omo <diwakar1998v@gmail.com>
2024-10-16 10:38:44 +0530

    triple modified

--- llvm/include/llvm/TargetParser/Triple.h
+++ llvm/include/llvm/TargetParser/Triple.h
@@ -107,6 +107,7 @@ public:
     renderscript32, // 32-bit RenderScript
     renderscript64, // 64-bit RenderScript
     ve,             // NEC SX-Aurora Vector Engine
+    tinygpu,
     LastArchType = ve
   };
   enum SubArchType {
